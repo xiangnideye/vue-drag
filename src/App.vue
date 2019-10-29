352841d370bfec5e2226670a2e82f66bc5da7d4c
zhangyin01 <zhangyin01@100tal.com>
2019-10-29 10:26:24 +0800

    vue-select

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="updata-body" @dragenter="dragenter($event)">
+    <div @drop="enentDrop($event)" @dragleave="dragleave($event)" @dragover="dragover($event)">
+      <div class="drag-box">{{drapText}}</div>
+    </div>
+    <ul>
+      <li v-for="(item, index) in fileData" :key="index">{{item.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Vue from 'vue';
+  export default {
+    name: 'app',
+    data() {
+      return {
+        drapText: '将文件拖拽到此处进行上传',
+        fileData: []
+      }
+    },
+    mounted() {
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    },
+    methods: {
+      enentDrop: function(e) {
+        console.log(e.dataTransfer.files)
+        e.stopPropagation();
+        e.preventDefault(); 
+        this.updataFun(e.dataTransfer.files)
+      },
+      dragleave(e) {
+        e.stopPropagation();
+        e.preventDefault();
+      },
+      dragenter(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        this.drapText = '松开鼠标完成上传';
+      },
+      dragover(e) {
+        e.stopPropagation();
+        e.preventDefault();
+      },
+      updataFun(data) {
+        let that = this;
+        for (let i = 0; i < data.length; i++) {
+          this.fileData.push(data[i])
+        }
+        this.drapText = '上传成功';
+        setTimeout(() => {
+          that.drapText = '将文件拖拽到此处进行上传';
+        }, 500);
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .updata-body {
+    margin: 50px auto;
+    width: 500px;
+  }
+  .drag-box {
+    width: 500px;
+    height: 200px;
+    line-height: 200px;
+    text-align: center;
+    border: 1px dashed #eee;
+  }
 </style>
